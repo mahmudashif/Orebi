7de952791254f885be22c72125613f54a8d4fc19
ashifmahmud <mdamahmud2000@gmail.com>
2024-09-09 00:26:56 +0600

    feat: orebi navbar added without resposive

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'dm': ['DM Sans', 'sans-serif'],
+    },
+    extend: {
+      container:"1604px",
+    },
+    colors: {
+      'primaryBg': '#979797',
+    },
+  },
+  plugins: [],
+}
