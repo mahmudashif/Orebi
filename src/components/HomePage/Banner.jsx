b8238cc0e70dc48e590a2ba636f18fcdc9debda1
ashifmahmud <mdamahmud2000@gmail.com>
2024-09-13 00:05:57 +0600

    feat: banner down part updated without responsive

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import banner from '/src/assets/banner.svg'
+import information from '/src/assets/information.svg'
 
 const Banner = () => {
   return (
+    <>
     <div>
         <img src={banner} alt="" />
     </div>
+    <div>
+        <img src={information} alt="" />
+    </div>
+    </>
   )
 }
 
